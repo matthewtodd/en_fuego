f249e1e96bf71a3a930dcd75d3ce403d960a15ee
Matthew Todd <matthew.todd@gmail.com>
2010-07-20 12:22:37 +0300

    Casting about. Try with feed-level link alternate.

--- lib/en_fuego/application.rb
+++ lib/en_fuego/application.rb
@@ -153,7 +153,8 @@ module EnFuego
           xml.id request.url
           xml.title 'En Fuego'
           xml.updated entries.first.updated
-          xml.link :rel => 'self', :href => request.url
+          xml.link :rel => 'self',      :href => request.url
+          xml.link :rel => 'alternate', :href => request.url[0...request.url.index(request.fullpath)]
 
           xml.author do
             xml.name 'DailyMile'
